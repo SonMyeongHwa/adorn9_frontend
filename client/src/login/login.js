590f7c8a80a30c1d24635718227076319c1e3a2c
ImGalim <dev.galim@gmail.com>
2023-10-10 06:11:01 +0900

    Feat: 로그인 로직 추가

--- client/src/login/login.js
+++ client/src/login/login.js
@@ -1,14 +1,33 @@
 // 정규식
 let emailReg =
 	/^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/;
+const passwordReg =
+	/^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$/;
 
-let userName = document.getElementById("exampleInputName");
-let userPhoneNum = document.getElementById("exampleInputPhoneNumber");
 let userEmail = document.getElementById("exampleInputEmail1");
 let userPw = document.getElementById("exampleInputPassword1");
 
 function handleLogin() {
-	// e.preventDefault();
+	if (userEmail.value == "") {
+		alert("이메일을 입력해주세요");
+		userEmail.focus();
+		return;
+	} else if (!emailReg.test(userEmail.value)) {
+		alert("이메일 형식이 올바르지 않습니다");
+		userEmail.focus();
+		return false;
+	}
+
+	if (userPw.value == "") {
+		alert("비밀번호를 입력해주세요");
+		userPw.focus();
+		return;
+	} else if (!passwordReg.test(userPw.value)) {
+		alert("비밀번호를 확인해주세요");
+		userPw.focus();
+		return false;
+	}
+
 	fetch("http://localhost:3000/api/v1/users/login", {
 		method: "POST",
 		header: {
@@ -21,8 +40,7 @@ function handleLogin() {
 	})
 		.then((response) => response.json())
 		.then((data) => {
-			console.log(data);
-			alert("로그인에 성공했습니다");
-			// navigate("/");
+			localStorage.getItem("TOKEN", data.accessToken);
+			alert("로그인 성공");
 		});
 }
