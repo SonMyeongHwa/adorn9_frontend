1e37493e8cd6f451f49199496aae38928b749680
elice61 <elice61@elice61ui-iMac.local>
2023-10-13 15:57:36 +0900

    Remove: 파일 삭제

--- client/src/usefulFunction.js
+++
@@ -1,9 +0,0 @@
-// 로그인 여부 확인 후 로그인시 원래 페이지로 이동
-export const checkLogin = () => {
-	const token = sessionStorage.getItem('token');
-	if (!token) {
-		const pathname = window.location.pathname;
-		const search = window.location.search;
-		window.location.replace(`/login?previouspage=${pathname + search}`);
-	}
-};
