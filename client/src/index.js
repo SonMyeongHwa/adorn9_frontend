346620e06378a87b2ae569279a37794bca4956b6
[SW6] 장보근 <elicespritte@gmail.com>
2023-10-14 07:07:07 +0900

    test

--- client/src/index.js
+++ client/src/index.js
@@ -21,7 +21,7 @@ const loginCheck = () => {
 		document.querySelector('.logout').onclick = function () {
 			localStorage.clear();
 			alert('로그아웃 되었습니다.');
-			window.location.href = '../main.html';
+			window.location.href = '/main.html';
 		};
 
 		document.querySelector(
