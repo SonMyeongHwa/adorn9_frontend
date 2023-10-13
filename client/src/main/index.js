c38f057932e44a891358b4ebeebf44262dfe80f4
woo7i <or2447221@gmail.com>
2023-10-13 14:50:28 +0900

    Fix:폴더 경로 수정

--- client/src/main/index.js
+++ client/src/main/index.js
@@ -1,13 +1,8 @@
-// nav
-// import { loginCheck, createLoginNav, createLogoutNav } from '../navbar.js';
 import apiModule from './api.js';
 import advertising from './advertise.js';
 import mainSlideModule from './main-slide.js';
 import createTheModal from '../modal/modal.js';
 
-loginCheck();
-createLoginNav();
-createLogoutNav();
 apiModule.fetch();
 advertising.advertise();
 mainSlideModule.makeClone();
